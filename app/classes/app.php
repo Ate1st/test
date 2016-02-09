25c7f06987b70f3361d7165ac93fb9c5897c33bd
User <User@WIN-8FCV7S84SV8>
2016-02-09 22:56:10 +0300

    del log from app

--- app/classes/app.php
+++ app/classes/app.php
@@ -14,18 +14,11 @@ class app
 
     public static function init()
     {
-        $start = microtime(true);
-        
-        //session_start();
-        
         config::set_config_path('configs');
         config::set_config('db_config');
         config::set_config('app_config');
-        
-        //if(!config::get_config('pdo'))
-        //{
-            db::Connect(config::get_config('db_config'));
-        //}
+
+        db::Connect(config::get_config('db_config'));
 
         config::set_config('pdo', db::get_pdo());
         
@@ -34,23 +27,14 @@ class app
         
         config::set_config('app_path', $_SERVER['DOCUMENT_ROOT']);
         config::set_config('bloks_new_path', config::get_config('app_path').'/bin/bloks');
-        
-        log::add(" app_init  -> \n ", $start);
 
     }
 
     public static function start()
     {
-
-        $start = microtime(true);
-
         request::init(config::get_config('app_config'));
-        
-        //print_r(request::get_params());
 
         controller::call(request::get_route(), request::get_params());
-        
-        log::add(" app_start  ->  \n", $start);
     }
 
 
